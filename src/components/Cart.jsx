0b0af28ccc9fa7eff6568d8eff895b02594788e1
zaenny <lovelyjan8@gmail.com>
2024-06-01 08:47:15 +0900

    모달창이 되지않음

---
+++ src/components/Cart.jsx
@@ -0,0 +1,30 @@
+import { useContext } from "react";
+
+import Modal from "./UI/modal";
+import CartContext from "../store/CartContext";
+import { currencyFormatter } from "../util/formatting";
+import Button from "./UI/Button";
+import UserProgressContext from "../store/UserProgressContext";
+
+export default function Cart(){
+    const cartCtx = useContext(CartContext);
+    const UserProgressCtx = useContext(UserProgressContext);
+
+    const cartTotal = cartCtx.items.reduce((totalPrice, item) => totalPrice + item.quantity * item.price, 0);
+    return(
+        <Modal className="cart" open={UserProgressCtx.progress === 'cart'}>
+            <h2>Your Cart</h2>
+            <ul>
+                {cartCtx.items.map(item => (
+                    <li key={item.id}>
+                        {item.name} - {item.quantity}
+                    </li>))}
+            </ul>
+            <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
+            <p className="modal-actions">
+                <Button textOnly>Close</Button>
+                <Button>Go To Checkout</Button>
+            </p>
+        </Modal>
+    );
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,20 @@ import Button from './UI/Button';
 import logoImg from '../assets/logo.jpg'
 import { useContext } from 'react';
 import CartContext from '../store/CartContext';
+import UserProgressContext from '../store/UserProgressContext';
 
 export default function Header(){
     
     const cartCtx = useContext(CartContext);
+    const userProgressCtx = useContext(UserProgressContext);
     // 내장 reduce메소드는 배열을 하나의 값으로 줄여주는데 즉 숫자 하나로 줄여줄수있다.
-    const totalCartItems = cartCtx.items.reduce(() => {}, 0);
+    const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
+        return totalNumberOfItems + item.quantity 
+    }, 0);
+
+    function handleShowCart(){
+        userProgressCtx.showCart();
+    }
 
     return(
         <header id="main-header">
@@ -16,8 +24,8 @@ export default function Header(){
                 <h1>React Food</h1>
             </div>
             <nav>
-                <Button textOnly>Cart (0)</Button>
+                <Button textOnly onClick={handleShowCart} >Cart ({totalCartItems})</Button>
             </nav>
         </header>
-    );
+    ); 
 }
